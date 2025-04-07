aeb87235ed3ecfb83422693c36b3de0adfc9938a
jihwanprogramer <kob882333@naver.com>
2025-04-07 15:46:49 +0900

    init

---
+++ src/main/java/org/example/newsfeed/NewSfeedApplication.java
@@ -0,0 +1,13 @@
+package org.example.newsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSfeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewSfeedApplication.class, args);
+    }
+
+}
