fef1f37129e4ee09ac1c432f818dd406f3242097
chaewon9999 <w0729@naver.com>
2025-04-07 19:57:40 +0900

    feat : 게시글 생성 구현 및 테스트 완료

--- src/main/java/org/example/newsfeed/post/controller/PostController.java
+++ src/main/java/org/example/newsfeed/post/controller/PostController.java
@@ -1,7 +1,13 @@
 package org.example.newsfeed.post.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.newsfeed.post.dto.CreateRequestDto;
+import org.example.newsfeed.post.dto.CreateResponseDto;
 import org.example.newsfeed.post.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<CreateResponseDto> create(@RequestBody CreateRequestDto requestDto) {
+
+        CreateResponseDto responseDto = postService.create(requestDto);
+
+        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
 }
--- src/main/java/org/example/newsfeed/post/service/PostService.java
+++ src/main/java/org/example/newsfeed/post/service/PostService.java
@@ -1,7 +1,11 @@
 package org.example.newsfeed.post.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.newsfeed.post.dto.CreateRequestDto;
+import org.example.newsfeed.post.dto.CreateResponseDto;
+import org.example.newsfeed.post.entity.Post;
 import org.example.newsfeed.post.repository.PostRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +13,13 @@ import org.springframework.stereotype.Service;
 public class PostService {
 
     private final PostRepository postRepository;
+
+    public CreateResponseDto create(CreateRequestDto requestDto) {
+
+        Post post = new Post(requestDto);
+
+        Post savedPost = postRepository.save(post);
+
+        return new CreateResponseDto(savedPost);
+    }
 }
