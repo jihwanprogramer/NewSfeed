c38417a41c5e58d61a7f7461927528982b30fb21
obt <byeongtaek12@naver.com>
2025-04-07 20:02:31 +0900

    UserRequestDto클래스) 생성 및 필드 선언
    ``

--- src/main/java/org/example/newsfeed/user/dto/UserRequestDto.java
+++ src/main/java/org/example/newsfeed/user/dto/UserRequestDto.java
@@ -1,4 +1,28 @@
 package org.example.newsfeed.user.dto;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class UserRequestDto {
+
+    private final Long id;
+
+    private final String name;
+
+    private final Integer age;
+
+    private final LocalDateTime createdAt;
+
+    private final LocalDateTime modifiedAt;
+
+
+    public UserRequestDto(Long id, String name, Integer age, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
 }
