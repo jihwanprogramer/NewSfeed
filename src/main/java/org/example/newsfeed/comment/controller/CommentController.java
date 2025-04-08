437e0df623c38d310089d3a0546b5b9fc1770b41
jihwanprogramer <kob882333@naver.com>
2025-04-08 15:53:40 +0900

    feat(comment): 댓글 관련 HTTP 요청 처리 기능 추가
    - saveComment: 댓글을 저장하는 POST 메서드 추가
    - findByPostId: 특정 게시글의 댓글을 조회하는 GET 메서드 추가
    - updateComment: 댓글을 수정하는 PUT 메서드 추가
    - deleteComment: 댓글을 삭제하는 DELETE 메서드 추가

--- src/main/java/org/example/newsfeed/comment/controller/CommentController.java
+++ src/main/java/org/example/newsfeed/comment/controller/CommentController.java
@@ -1,10 +1,9 @@
 package org.example.newsfeed.comment.controller;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.example.newsfeed.comment.dto.CommentResponseDto;
-import org.example.newsfeed.comment.dto.CommentRequestDto;
 import org.example.newsfeed.comment.dto.CommentSaveRequestDto;
+import org.example.newsfeed.comment.dto.CommentUpdateRequestDto;
 import org.example.newsfeed.comment.service.CommentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,20 +19,41 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/{post_id}/comments")
-    public ResponseEntity<CommentResponseDto> save(
+    public ResponseEntity<CommentResponseDto> saveComment(
+            @SessionAttribute(name = "LOGIN_USER") Long userId,
             @PathVariable Long post_id,
             @RequestBody CommentSaveRequestDto commentSaveRequestDto
-    ){
-        CommentResponseDto save = commentService.save(post_id,commentSaveRequestDto);
+    ) {
+        CommentResponseDto save = commentService.save(userId, post_id, commentSaveRequestDto);
 
-        return new ResponseEntity<>(save,HttpStatus.CREATED);
+        return new ResponseEntity<>(save, HttpStatus.CREATED);
     }
 
     @GetMapping("/{post_id}/comments")
-    public ResponseEntity<List<CommentResponseDto>> findAll(@PathVariable Long id){
-        List<CommentResponseDto> commentResponseDtoList = commentService.findByPost(id);
+    public ResponseEntity<List<CommentResponseDto>> findByPostId(@PathVariable Long post_id) {
+        List<CommentResponseDto> commentResponseDtoList = commentService.findByPost(post_id);
 
-        return new ResponseEntity<>(commentResponseDtoList,HttpStatus.OK);
+        return new ResponseEntity<>(commentResponseDtoList, HttpStatus.OK);
+    }
+
+    @PutMapping("/comments/{comment_id}")
+    public ResponseEntity<CommentResponseDto> updateComment(
+            @SessionAttribute(name = "LOGIN_USER") Long userId,
+            @PathVariable Long comment_id,
+            @RequestBody CommentUpdateRequestDto commentUpdateRequestDto) {
+
+        CommentResponseDto updateComment = commentService.updateComment(comment_id, userId, commentUpdateRequestDto);
+
+        return new ResponseEntity<>(updateComment, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/comments/{comment_id}")
+    public ResponseEntity<Void> deleteComment(
+            @SessionAttribute(name = "LOGIN_USER") Long userId,
+            @PathVariable Long comment_id) {
+        commentService.delete(comment_id, userId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
