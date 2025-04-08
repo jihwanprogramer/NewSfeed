ce53a764435e988cf05981587ad221802abd302a
jihwanprogramer <kob882333@naver.com>
2025-04-08 10:41:41 +0900

    code save

--- src/main/java/org/example/newsfeed/comment/controller/CommentController.java
+++ src/main/java/org/example/newsfeed/comment/controller/CommentController.java
@@ -1,4 +1,26 @@
 package org.example.newsfeed.comment.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.example.newsfeed.comment.dto.CommentReponseDto;
+import org.example.newsfeed.comment.dto.CommentRequestDto;
+import org.example.newsfeed.comment.service.CommentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/board")
+@RequiredArgsConstructor
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping{"/{post_id}/comments"}
+    public ResponseEntity<CommentReponseDto> save(
+            @PathVariable Long post_id,
+            @RequestBody CommentRequestDto commentRequestDto
+    ){
+
+    }
+
 }
--- src/main/java/org/example/newsfeed/comment/entity/Comment.java
+++ src/main/java/org/example/newsfeed/comment/entity/Comment.java
@@ -1,9 +1,27 @@
 package org.example.newsfeed.comment.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.newsfeed.common.entity.BaseEntity;
 
 @Getter
 @Entity
-public class Comment {
+@NoArgsConstructor
+public class Comment extends BaseEntity
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
