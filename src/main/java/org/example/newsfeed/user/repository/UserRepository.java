0469d7715deccdfefbd65be8ade927b5b9324432
obt <byeongtaek12@naver.com>
2025-04-08 10:22:50 +0900

    refactor: UserRepository인터페이스) 제네릭값 수정
    ``

--- src/main/java/org/example/newsfeed/user/repository/UserRepository.java
+++ src/main/java/org/example/newsfeed/user/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package org.example.newsfeed.user.repository;
 
-import jakarta.persistence.Id;
 import org.example.newsfeed.user.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<Users,Id> {
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
 }
