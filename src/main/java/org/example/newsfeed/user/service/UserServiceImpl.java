cea11e33dc8eeeaa6c1059580a6b0e5a1e00a8e9
obt <byeongtaek12@naver.com>
2025-04-08 10:18:24 +0900

    feat: UserServiceImpl클래스) 생성 및 메서드 추가
    회원가입 메서드와 아이디로 유저를 찾는 메서드 추가

--- src/main/java/org/example/newsfeed/user/service/UserServiceImpl.java
+++ src/main/java/org/example/newsfeed/user/service/UserServiceImpl.java
@@ -1,4 +1,47 @@
 package org.example.newsfeed.user.service;
 
-public class UserServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.example.newsfeed.user.dto.UserResponseDto;
+import org.example.newsfeed.user.entity.Users;
+import org.example.newsfeed.user.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserResponseDto signUp(String name, Integer age, String email, String password) {
+
+        Users users = new Users(name, age, email, password);
+
+        Users savedUser = userRepository.save(users);
+
+        return new UserResponseDto(savedUser.getId(), savedUser.getName(),savedUser.getAge(),savedUser.getCreatedAt(),
+                savedUser.getModifiedAt());
+
+    }
+
+
+
+
+    @Override
+    public UserResponseDto findUserById(Long id) {
+
+        Users findUser = userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                "유저가 없습니다"));
+
+        return new UserResponseDto(findUser.getId(), findUser.getName(), findUser.getAge(), findUser.getCreatedAt(),
+                findUser.getModifiedAt());
+
+    }
+
+
 }
