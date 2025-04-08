cc9a34f1591324bfd35e86ef0e5150010eceadb2
obt <byeongtaek12@naver.com>
2025-04-08 12:57:47 +0900

    feat: UserRepository인터페이스) default 메서드 추가
    service에서 null 체크 하던 부분 repository로 옮김

--- src/main/java/org/example/newsfeed/user/repository/UserRepository.java
+++ src/main/java/org/example/newsfeed/user/repository/UserRepository.java
@@ -2,8 +2,18 @@ package org.example.newsfeed.user.repository;
 
 import org.example.newsfeed.user.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findUserByName(String name);
+
+    default Users findUserByIdOrElseThrow(Long id) {
+        return findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                "id를 가진 회원이 없습니다"));
+    }
 }
