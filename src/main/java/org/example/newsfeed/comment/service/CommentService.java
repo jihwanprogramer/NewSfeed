d4ef1f63601c43636a062922e5c6b31f50707127
jihwanprogramer <kob882333@naver.com>
2025-04-08 15:53:58 +0900

    feat(comment): 댓글 비즈니스 로직 구현
    - save: 댓글 저장 로직 추가
    - findByPost: 게시글에 대한 댓글 조회 로직 추가
    - updateComment: 댓글 수정 로직 추가
    - delete: 댓글 삭제 로직 추가

--- src/main/java/org/example/newsfeed/comment/service/CommentService.java
+++ src/main/java/org/example/newsfeed/comment/service/CommentService.java
@@ -3,14 +3,13 @@ package org.example.newsfeed.comment.service;
 import lombok.RequiredArgsConstructor;
 import org.example.newsfeed.comment.dto.CommentResponseDto;
 import org.example.newsfeed.comment.dto.CommentSaveRequestDto;
+import org.example.newsfeed.comment.dto.CommentUpdateRequestDto;
 import org.example.newsfeed.comment.entity.Comment;
 import org.example.newsfeed.comment.repository.CommentRepository;
 import org.example.newsfeed.post.entity.Post;
 import org.example.newsfeed.post.repository.PostRepository;
-import org.example.newsfeed.user.dto.UserResponseDto;
 import org.example.newsfeed.user.entity.Users;
 import org.example.newsfeed.user.repository.UserRepository;
-import org.example.newsfeed.user.service.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,19 +21,22 @@ import java.util.stream.Collectors;
 public class CommentService {
 
     private final CommentRepository commentRepository;
-    private final UserService userService;
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
     @Transactional
-    public CommentResponseDto save(Long postId, CommentSaveRequestDto dto) {
+    public CommentResponseDto save(Long userId, Long postId, CommentSaveRequestDto dto) {
 
         Post findPost = postRepository.findByIdOrElseThrow(postId);
-        Comment comment = new Comment(findPost,dto.getContent());
+
+        Users user = userRepository.findUserByIdOrElseThrow(userId);
+        Comment comment = new Comment(findPost, user, dto.getContent());
         commentRepository.save(comment);
 
         return new CommentResponseDto(
                 comment.getId(),
                 findPost.getId(),
+                user.getId(),
                 comment.getContent(),
                 comment.getCreateAt(),
                 comment.getUpdatedAt()
@@ -42,16 +44,47 @@ public class CommentService {
 
     }
 
+    @Transactional
     public List<CommentResponseDto> findByPost(Long id) {
-        Post post =postRepository.findByIdOrElseThrow(id);
+        Post post = postRepository.findByIdOrElseThrow(id);
         List<Comment> comments = commentRepository.findByPost(post);
         return comments.stream().map(comment -> new CommentResponseDto(
-                        comment.getId(),
-                        post.getId(),
-                        comment.getContent(),
-                        comment.getCreateAt(),
-                        comment.getUpdatedAt()
-                )).collect(Collectors.toList());
+                comment.getId(),
+                post.getId(),
+                comment.getUser().getId(),
+                comment.getContent(),
+                comment.getCreateAt(),
+                comment.getUpdatedAt()
+        )).collect(Collectors.toList());
     }
 
+    @Transactional
+    public CommentResponseDto updateComment(Long commentId, Long userId, CommentUpdateRequestDto commentUpdateRequestDto) {
+        Comment comment = commentRepository.findByIdOrElseThrow(commentId);
+
+        if (!comment.getUser().getId().equals(userId)) {
+            throw new IllegalArgumentException();
+        }
+
+        comment.update(commentUpdateRequestDto.getContent());
+        return new CommentResponseDto(
+                comment.getId(),
+                comment.getPost().getId(),
+                comment.getUser().getId(),
+                comment.getContent(),
+                comment.getCreateAt(),
+                comment.getUpdatedAt()
+        );
+
+    }
+
+    @Transactional
+    public void delete(Long commentId, Long userId) {
+        Comment comment = commentRepository.findByIdOrElseThrow(commentId);
+
+        if (!comment.getUser().getId().equals(userId)) {
+            throw new IllegalArgumentException();
+        }
+        commentRepository.delete(comment);
+    }
 }
