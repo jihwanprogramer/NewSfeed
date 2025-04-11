95127be880251cb5eab159eccd9e1d2e47f6df07
chaewon9999 <w0729@naver.com>
2025-04-11 15:19:32 +0900

    fix : 보드 서비스 인터페이스 접근제어자 수정

--- src/main/java/org/example/newsfeed/board/service/BoardService.java
+++ src/main/java/org/example/newsfeed/board/service/BoardService.java
@@ -3,31 +3,24 @@ package org.example.newsfeed.board.service;
 import org.example.newsfeed.board.dto.*;
 import org.example.newsfeed.user.dto.UserResponseDto;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BoardService {
 
-    public CreateResponseDto createBoard(CreateRequestDto requestDto, UserResponseDto userResponseDto);
+    CreateResponseDto createBoard(CreateRequestDto requestDto, UserResponseDto userResponseDto);
 
-    public List<BoardResponseDto> findAll();
+    List<BoardResponseDto> findAll();
 
-    public Page<PageResponseDto> findAllPage(int page);
+    Page<PageResponseDto> findAllPage(int page);
 
-    public BoardResponseDto findBoardById(Long id);
+    BoardResponseDto findBoardById(Long id);
 
-    public BoardResponseDto updateBoard(Long id, UpdateRequestDto requestDto, UserResponseDto userResponseDto);
+    BoardResponseDto updateBoard(Long id, UpdateRequestDto requestDto, UserResponseDto userResponseDto);
 
-    public Page<PageResponseDto> sortedByModifiedAt(int page);
+    Page<PageResponseDto> sortedByModifiedAt(int page);
 
-    public Page<PageResponseDto> findBoardByPeriod(PeriodRequestDto requestDto, int page);
+    Page<PageResponseDto> findBoardByPeriod(PeriodRequestDto requestDto, int page);
 
-    public Page<LikesResponseDto> sortedByLikes(int page);
+    Page<LikesResponseDto> sortedByLikes(int page);
 
 }
