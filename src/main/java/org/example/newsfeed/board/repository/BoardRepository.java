5ae7afad8b0212a76b31e92ab9ac4debe7131e57
obt <byeongtaek12@naver.com>
2025-04-11 16:19:43 +0900

    feat: BoardRepository인터페이스) 메서드 추가
    유저 받아서 삭제하는 메서드 추가

--- src/main/java/org/example/newsfeed/board/repository/BoardRepository.java
+++ src/main/java/org/example/newsfeed/board/repository/BoardRepository.java
@@ -3,6 +3,7 @@ package org.example.newsfeed.board.repository;
 
 import org.example.newsfeed.board.entity.Board;
 import org.example.newsfeed.exception.MisMatchUserException;
+import org.example.newsfeed.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,6 +13,8 @@ import java.time.LocalDateTime;
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     Page<Board> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate, PageRequest pageable);
+
+    void deleteAllByUser(User user);
 
     //아이디 확인 후 존재하지 않을 경우 예외
     default Board findByIdOrElseThrow(Long id) {
