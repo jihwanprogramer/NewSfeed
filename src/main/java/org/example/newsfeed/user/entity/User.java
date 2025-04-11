4c87fbbbe1a276623944c5ba58c71a3edba786e1
obt <byeongtaek12@naver.com>
2025-04-11 14:49:46 +0900

    refactor: User클래스) setter 제거 및 메서드 추가
    setter를 사용하지 않고 직접 엔티티에서 다루는 게 좋다고 하여 수정 메서드에 있는 로직을 엔티티로 옮김,
    엔티티와 밀접한 관련이 있는 예외처리를 엔티티로 옮김

--- src/main/java/org/example/newsfeed/user/entity/User.java
+++ src/main/java/org/example/newsfeed/user/entity/User.java
@@ -2,11 +2,11 @@ package org.example.newsfeed.user.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
+import org.example.newsfeed.common.config.PasswordEncoder;
+import org.example.newsfeed.exception.WrongPasswordException;
 
 @Entity
 @Getter
-@Setter
 @Table(name = "users")
 public class User extends BaseEntity{
 
@@ -33,6 +33,42 @@ public class User extends BaseEntity{
         this.age = age;
         this.email=email;
         this.password=password;
+    }
+
+    public void passwordMatch(String password, PasswordEncoder passwordEncoder) {
+
+        if (!passwordEncoder.matches(password,this.password)) {
+            throw new WrongPasswordException("비밀번호가 일치하지 않습니다.");
+        }
+
+    }
+
+
+    public void notNullUpdate(String name, Integer age, String email, String password, String newPassword,
+                              String checkNewPassword, PasswordEncoder passwordEncoder) {
+
+        if (name != null) {
+            this.name = name;
+        }
+
+        if (age != null) {
+            this.age = age;
+        }
+
+        // 널은 허용, 빈 문자열은 허용X
+        if (email != null && !email.isEmpty()) {
+            this.email=email;
+        }
+
+        // 둘 다 넣어야 변경
+        if (newPassword != null && checkNewPassword != null) {
+
+            passwordMatch(password, passwordEncoder);
+
+            this.password = passwordEncoder.encode(newPassword);
+
+        }
+
     }
 
     public boolean isSameUser(Long id){
